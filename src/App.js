6c7f7a57e590b41dac9ada5ce41b9141fac1c735
arijhajji <61475226+arijhajji-1@users.noreply.github.com>
2023-03-13 15:37:15 +0100

    final touches

---
+++ src/App.js
@@ -0,0 +1,42 @@
+//import logo from './logo.svg';
+import './App.css';
+import React, { useState, useEffect } from "react";
+import { Suspense } from 'react';
+import {  Route, Routes } from 'react-router-dom';
+
+
+
+
+const Sidebar = React.lazy(() => import('./Components/Pages/Sidebar'))
+const Footer = React.lazy(() => import('./Components/Pages/Footer'))
+const Navbar = React.lazy(()=>import('./Components/Pages/Navbar'))
+const Dashboard = React.lazy(()=>import('./Components/Pages/Dashboard'))
+const SignIn = React.lazy(()=>import('./Components/Admin/SignIn'))
+const ListUsers = React.lazy(()=>import('./Components/Admin/ListUsers'))
+
+
+
+function App() {
+  return (
+<div className='MyApp'>
+
+<Suspense fallback={<div>Loading...</div>}>
+<Navbar/>
+    <Sidebar/>
+     
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+        
+          <Route path="/signin" element={<SignIn/>}/>
+          <Route path="/list" element={<ListUsers/>}/>
+         
+
+        </Routes>
+<Footer/>
+      </Suspense>
+
+    </div>
+  );
+}
+
+export default App;
